120f16ff500af9a883365f365676d04e0eef26dc
ujin <dbwlsxkal95@gmail.com>
2022-06-15 23:21:39 +0900

    홍유진 7일차 과제 제출합니다

--- ujin/programmers/day06/모의고사.js
+++ ujin/programmers/day06/모의고사.js
@@ -1,12 +1,12 @@
 function solution(answers) {
   const answer = [];
-  const people1 = [1, 2, 3, 4, 5];
-  const people2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const people3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const supo1 = [1, 2, 3, 4, 5];
+  const supo2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const supo3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  const number1 = answers.filter((a, i) => a === people1[i % 5]).length;
-  const number2 = answers.filter((a, i) => a === people2[i % 8]).length;
-  const number3 = answers.filter((a, i) => a === people3[i % 10]).length;
+  const number1 = answers.filter((a, i) => a === supo1[i % 5]).length;
+  const number2 = answers.filter((a, i) => a === supo2[i % 8]).length;
+  const number3 = answers.filter((a, i) => a === supo3[i % 10]).length;
   const max = Math.max(number1, number2, number3);
 
   if (number1 === max) answer.push(1);
